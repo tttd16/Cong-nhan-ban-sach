8e1ea1e1b5d4ad59cff52aad4e32e18a17c9c3cf
tttd16 <tiendung.work16@gmail.com>
2022-09-16 22:30:44 +0700

    dung update

---
+++ assets/js/app.js
@@ -0,0 +1,204 @@
+/** COUNTER UP */
+function subscriber() {
+    let counts = setInterval(updated);
+    let upto = 0;
+    function updated() {
+        var subscriber = document.getElementById("subscriber");
+        subscriber.innerHTML = ++upto;
+        if (upto == 4500) {
+        clearInterval(counts);
+        }
+    }
+}
+
+function download() {
+    let counts = setInterval(updated, 100);
+    let upto = 0;
+    function updated() {
+        var download = document.getElementById("download");
+        download.innerHTML = ++upto;
+        if (upto == 200) {
+        clearInterval(counts);
+        }
+    }
+}
+
+function product() {
+    let counts = setInterval(updated);
+    let upto = 0;
+    function updated() {
+        var product = document.getElementById("product");
+        product.innerHTML = ++upto;
+        if (upto == 1100) {
+        clearInterval(counts);
+        }
+    }
+}
+
+function ebook() {
+    let counts = setInterval(updated, 100);
+    let upto = 0;
+    function updated() {
+        var ebook = document.getElementById("ebook");
+        ebook.innerHTML = ++upto;
+        if (upto == 30) {
+        clearInterval(counts);
+        }
+    }
+}
+
+subscriber();
+download();
+product();
+ebook();
+
+/**===================================================================== */
+
+/** Slideshow automatic */
+let slideIndex = 1;
+showSlides();
+function showSlides() {
+    let slides = document.getElementsByClassName("mySlide");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].classList.remove("appear");
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex - 1].classList.add("appear");
+
+    setTimeout(showSlides, 2000);
+}
+
+/** ======================================================================= */
+
+/**================FEEDBACK SLIDE========================================== */
+let indexFeedback = 1;
+function currentSlide(n) {
+    showSlideFeedback(indexFeedback = n);
+}
+
+function showSlideFeedback(n) {
+    let slides = document.getElementsByClassName("feedback-detail");
+    let dots = document.getElementsByClassName("owl-dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {indexFeedback = slides.length}
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].classList.add("hide");
+    }
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].classList.remove("active");
+    }
+    slides[indexFeedback - 1].classList.remove("hide");
+    dots[indexFeedback - 1].classList.add("active"); 
+}
+/** ======================================================================= */
+// let next = document.getElementById('next');
+// let prev = document.getElementById('prev');
+// let books = document.querySelector('.books');
+
+// let first_card_clone = books.children[0].cloneNode(true);
+// let last_card_clone = books.children[books.children.length - 1].cloneNode(true);
+
+// books.insertBefore(last_card_clone, books.children[0]);
+// books.appendChild(first_card_clone);
+
+// books.style.transistionDuration = "0.0s";
+// books.style.transform = `translate(-350px)`;
+
+// let current_book = 1;
+
+// next.addEventListener('click', () => {
+//     if (current_book < books.children.length - 1) {
+//         current_book++;
+
+//         books.style.transistionDuration = "0.5s";
+//         books.style.transform = `translate(-${current_book * 350}px)`;
+
+//         if (current_book === books.children.length - 1) {
+//             setTimeout(() => {
+//                 books.style.transistionDuration = "0.0s";
+//                 books.style.transform = `translate(-350px)`;
+//                 current_book = 1;
+//             }, 600);
+//         }
+//     } else {
+//         return;
+//     }
+// })
+
+// prev.addEventListener('click', () => {
+//     if (current_book > 0) {
+//         current_book--;
+
+//         books.style.transistionDuration = "0.5s";
+//         books.style.transform = `translate(-${current_book * 350}px)`;
+
+//         if (current_book === 0) {
+//             setTimeout(() => {
+//                 books.style.transistionDuration = "0.0s";
+//                 books.style.transform = `${(books.children.length - 2)*350}px`;
+//                 current_book = books.children.length-2;
+//             }, 600);
+//         }
+
+//     } else {
+//         return;
+//     }
+// })
+
+window.addEventListener("load", function() {
+    const sliderMain = document.querySelector(".books");
+    const sliderItems = document.querySelectorAll(".book-item");
+    const nextBtn = document.getElementById("next");
+    const prevBtn = document.getElementById("prev");
+    const sliderItemWidth = sliderItems[0].offsetWidth;
+    const slidesLength = sliderItems.length;
+    let positionX = 0;
+    let index = 0;
+
+    nextBtn.addEventListener('click', function() {
+        handleChangeSlide(1);
+    });
+    prevBtn.addEventListener('click', function() {
+        handleChangeSlide(-1);
+    });
+
+    function handleChangeSlide(direction) {
+        if (direction === 1) {
+            if (index >= slidesLength - 1) {
+                index = slidesLength - 1;
+                return;
+            }
+            positionX = positionX - sliderItemWidth;
+            sliderMain.style = `transform: translateX(${positionX}px)`;
+            index++;
+        } else if (direction === -1) {
+            if (index <= 0) {
+                index = 0;
+                return;
+            };
+            positionX = positionX + sliderItemWidth;
+            sliderMain.style = `transform: translateX(${positionX}px)`;
+            index--;
+        }
+    }
+});
+
+/**================BOOK SLIDE========================================== */
+
+/** ============================SCROLL TOP=========================================== */
+let mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+function scrollToTop() {
+    window.scrollTo(0, 0);
+}
+/** ======================================================================= */
